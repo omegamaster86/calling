fda4d0f3976ea6194693a3c454d4108e1b518356
omegamaster86 <masya868686@gmail.com>
2024-01-05 18:33:01 +0900

    各コンポーネントを追加

--- front/pages/dashbord.tsx
+++ front/pages/dashbord.tsx
@@ -1,7 +1,15 @@
+import { Header } from "./components/Header";
+import { Filter } from "./components/Filter";
+import { AttackStatus } from "./components/AttackStatus";
+import { AttackLog } from "./components/AttackLog";
 
 export default function Dashbord(){
     return(
-        <h1>ようこそ</h1>
+        <div>
+            <Header/>
+            <Filter/>
+            <AttackStatus/>
+            <AttackLog/>
+        </div>
     )
-
 }
