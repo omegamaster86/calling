a6114cc56b5cdefe068e12c25530bf0871fcc511
omegamaster86 <masya868686@gmail.com>
2024-02-17 19:57:35 +0900

    架電履歴の日付の表示方法を変更

--- front/components/AttackLog/AttackLogCallHistory/AttackLogCallHistory.tsx
+++ front/components/AttackLog/AttackLogCallHistory/AttackLogCallHistory.tsx
@@ -2,14 +2,30 @@ import { useCompanyAndAttackLogsData } from './useSWRAttackLog';
 import { useRouter } from 'next/router';
 import { Company } from '@/types/interface';
 
+
 export const AttackLogCallHistory = () => {
   const { mergedData, isLoading, isError } = useCompanyAndAttackLogsData();
   const router = useRouter();
   const { company: companyId } = router.query; 
+  const formatDateTime = ((dateTimeStr: string) => {
+    if (!Date.parse(dateTimeStr)) {
+      return '日時フォーマット変更前'; // 無効な日時の場合は 'N/A' を返す
+    }
+    const dateObj = new Date(dateTimeStr);
+    const formattedDate = new Intl.DateTimeFormat('ja-JP', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    }).format(dateObj);
+    return formattedDate; // '2024/02/17 17:52' の形式で返す
+  });
   
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading data</div>;
   const filteredData = mergedData.filter((company: Company) => company.id?.toString() === companyId);
+  
 
   return (
     <div className='mt-5 mx-auto'>
@@ -20,6 +36,7 @@ export const AttackLogCallHistory = () => {
             <tr>
                 <th className='border-2 w-44'>アタック日</th>
                 <th className='border-2 w-44'>架電結果</th>
+                {/* // TODO　対話内容を文字数に応じてポップアップかスライド形式で表示するか検討、優先度低め */}
                 <th className='border-2 w-44'>対話内容</th>
             </tr>
             </thead>
@@ -27,7 +44,7 @@ export const AttackLogCallHistory = () => {
             {filteredData.map((company, index) => (
                company.attackLogs.map((log, logIndex) => (
                 <tr key={`${index}-${logIndex}`} className={logIndex % 2 === 0 ? 'bg-white' : 'bg-gray-100'}>
-                  <td className='border-2 text-nowrap truncate'>{log.calling_day}</td>
+                  <td className='border-2 text-nowrap truncate'>{formatDateTime(log.calling_start)}</td>
                   <td className='border-2 truncate'>{log.call_result}</td>
                   <td className='border-2 truncate'>{log.call_content}</td>
                 </tr>
