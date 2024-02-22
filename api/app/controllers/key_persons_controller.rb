6cd96997e35ebb98c6ac152cdf390b30e53f737f
omegamaster86 <masya868686@gmail.com>
2024-02-22 20:31:43 +0900

    不要なコメント、ファイル削除

--- api/app/controllers/key_persons_controller.rb
+++ api/app/controllers/key_persons_controller.rb
@@ -14,11 +14,6 @@ class KeyPersonsController < ApplicationController
         end
     end
 
-    # アタックログで使用する可能性があるので、一旦後回し
-    # def edit
-    #     @key_person = KeyPerson.find(params[:id])
-    # end
-
     private
 
     def key_person_params
--- api/app/models/add_company.rb
+++
@@ -1,2 +0,0 @@
-class AddCompany < ApplicationRecord
-end
