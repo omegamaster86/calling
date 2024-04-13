425807f4ee5ba727d014cf6918959423562a3250
omegamaster86 <masya868686@gmail.com>
2024-04-13 16:23:29 +0900

    railwayでの変更を戻す

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -42,8 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # 下記追加
-  config.hosts << "https://calling-production.up.railway.app"
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
