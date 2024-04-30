5dfc45c23bddbaf697214c86dc4411de6848db14
omegamaster86 <masya868686@gmail.com>
2024-04-30 20:38:52 +0900

    案件管理用ページ、営業担当者成績ページへを追加

---
+++ front/pages/apoint-list.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ApointList = () => {
+  return (
+    <div>apoint-list</div>
+  )
+}
+
+export default ApointList
---
+++ front/pages/salesman-result.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalesmanResult = () => {
+  return (
+    <div>salesman-result</div>
+  )
+}
+
+export default SalesmanResult
