581a7e2fb9b2698cc24dec44a01fbe4b74779fdf
omegamaster86 <masya868686@gmail.com>
2024-05-07 20:25:36 +0900

    見本の設定

---
+++ front/components/ApointList/Icons.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+
+export const PlusIcon = () => {
+	return (
+		<div>
+			<AddIcon />
+		</div>
+	);
+};
+
+export const Delete = () => {
+	return (
+		<div>
+			<DeleteIcon />
+		</div>
+	);
+};
---
+++ front/components/ApointList/KanbanBoard.tsx
@@ -0,0 +1,285 @@
+import { PlusIcon } from "./Icons";
+import { useMemo, useState, useEffect } from "react";
+import type { Column, Id, Task } from "./types";
+import ColumnContainer from "./ColumnContainer";
+import {
+	DndContext,
+	DragOverlay,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from "@dnd-kit/core";
+import type {
+	DragEndEvent,
+	DragOverEvent,
+	DragStartEvent,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+import TaskCard from "./TaskCard";
+
+const defaultCols: Column[] = [
+	{
+		id: "1",
+		title: "商談前",
+	},
+	{
+		id: "2",
+		title: "1次商談",
+	},
+	{
+		id: "3",
+		title: "2次商談",
+	},
+	{
+		id: "4",
+		title: "3次商談",
+	},
+	{
+		id: "5",
+		title: "成約",
+	},
+	{
+		id: "6",
+		title: "失注",
+	},
+];
+
+const defaultTasks: Task[] = [
+	{
+		id: "1",
+		columnId: "todo",
+		content: "List admin APIs for dashboard",
+	},
+	{
+		id: "2",
+		columnId: "todo",
+		content:
+			"Develop user registration functionality with OTP delivered on SMS after email confirmation and phone number confirmation",
+	},
+];
+
+function KanbanBoard() {
+	const [columns, setColumns] = useState<Column[]>(defaultCols);
+	const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+	const [isClient, setIsClient] = useState(false);
+	const [tasks, setTasks] = useState<Task[]>(defaultTasks);
+	const [activeColumn, setActiveColumn] = useState<Column | null>(null);
+	const [activeTask, setActiveTask] = useState<Task | null>(null);
+	const sensors = useSensors(
+		useSensor(PointerSensor, {
+			activationConstraint: {
+				distance: 10,
+			},
+		}),
+	);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	return (
+		<div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+			<DndContext
+				sensors={sensors}
+				onDragStart={onDragStart}
+				onDragEnd={onDragEnd}
+				onDragOver={onDragOver}
+			>
+				<div className="m-auto flex gap-4">
+					<div className="flex gap-4">
+						<SortableContext items={columnsId}>
+							{columns.map((col) => (
+								<ColumnContainer
+									key={col.id}
+									column={col}
+									deleteColumn={deleteColumn}
+									updateColumn={updateColumn}
+									createTask={createTask}
+									deleteTask={deleteTask}
+									updateTask={updateTask}
+									tasks={tasks.filter((task) => task.columnId === col.id)}
+								/>
+							))}
+						</SortableContext>
+					</div>
+					{/* <button
+						type="button"
+						onClick={() => {
+							createNewColumn();
+						}}
+						className=" h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
+						<PlusIcon />
+						Add Column
+					</button> */}
+				</div>
+
+				{isClient
+					? createPortal(
+							<DragOverlay>
+								{activeColumn ? (
+									<ColumnContainer
+										column={activeColumn}
+										deleteColumn={deleteColumn}
+										updateColumn={updateColumn}
+										createTask={createTask}
+										deleteTask={deleteTask}
+										updateTask={updateTask}
+										tasks={tasks.filter(
+											(task) => task.columnId === activeColumn.id,
+										)}
+									/>
+								) : activeTask ? (
+									<TaskCard
+										task={activeTask}
+										deleteTask={deleteTask}
+										updateTask={updateTask}
+									/>
+								) : null}
+							</DragOverlay>,
+							document.body,
+					  )
+					: null}
+			</DndContext>
+		</div>
+	);
+
+	function createTask(columnId: Id) {
+		const newTask: Task = {
+			id: generateId(),
+			columnId,
+			content: `Task ${tasks.length + 1}`,
+		};
+
+		setTasks([...tasks, newTask]);
+	}
+
+	function deleteTask(id: Id) {
+		const newTasks = tasks.filter((task) => task.id !== id);
+		setTasks(newTasks);
+	}
+
+	function updateTask(id: Id, content: string) {
+		const newTasks = tasks.map((task) => {
+			if (task.id !== id) return task;
+			return { ...task, content };
+		});
+
+		setTasks(newTasks);
+	}
+
+	// function createNewColumn() {
+	// 	const columnToAdd: Column = {
+	// 		id: generateId(),
+	// 		title: `Column ${columns.length + 1}`,
+	// 	};
+
+	// 	setColumns([...columns, columnToAdd]);
+	// }
+
+	function deleteColumn(id: Id) {
+		const filteredColumns = columns.filter((col) => col.id !== id);
+		setColumns(filteredColumns);
+
+		const newTasks = tasks.filter((t) => t.columnId !== id);
+		setTasks(newTasks);
+	}
+
+	function updateColumn(id: Id, title: string) {
+		const newColumns = columns.map((col) => {
+			if (col.id !== id) return col;
+			return { ...col, title };
+		});
+
+		setColumns(newColumns);
+	}
+
+	function onDragStart(event: DragStartEvent) {
+		if (event.active.data.current?.type === "Column") {
+			setActiveColumn(event.active.data.current.column);
+			return;
+		}
+
+		if (event.active.data.current?.type === "Task") {
+			setActiveTask(event.active.data.current.task);
+			return;
+		}
+	}
+
+	function onDragEnd(event: DragEndEvent) {
+		setActiveColumn(null);
+		setActiveTask(null);
+
+		const { active, over } = event;
+		if (!over) return;
+
+		const activeId = active.id;
+		const overId = over.id;
+
+		if (activeId === overId) return;
+
+		const isActiveAColumn = active.data.current?.type === "Column";
+		if (!isActiveAColumn) return;
+
+		console.log("DRAG END");
+
+		setColumns((columns) => {
+			const activeColumnIndex = columns.findIndex((col) => col.id === activeId);
+
+			const overColumnIndex = columns.findIndex((col) => col.id === overId);
+
+			return arrayMove(columns, activeColumnIndex, overColumnIndex);
+		});
+	}
+
+	function onDragOver(event: DragOverEvent) {
+		const { active, over } = event;
+		if (!over) return;
+
+		const activeId = active.id;
+		const overId = over.id;
+
+		if (activeId === overId) return;
+
+		const isActiveATask = active.data.current?.type === "Task";
+		const isOverATask = over.data.current?.type === "Task";
+
+		if (!isActiveATask) return;
+
+		// Im dropping a Task over another Task
+		if (isActiveATask && isOverATask) {
+			setTasks((tasks) => {
+				const activeIndex = tasks.findIndex((t) => t.id === activeId);
+				const overIndex = tasks.findIndex((t) => t.id === overId);
+
+				if (tasks[activeIndex].columnId !== tasks[overIndex].columnId) {
+					// Fix introduced after video recording
+					tasks[activeIndex].columnId = tasks[overIndex].columnId;
+					return arrayMove(tasks, activeIndex, overIndex - 1);
+				}
+
+				return arrayMove(tasks, activeIndex, overIndex);
+			});
+		}
+
+		const isOverAColumn = over.data.current?.type === "Column";
+
+		// Im dropping a Task over a column
+		if (isActiveATask && isOverAColumn) {
+			setTasks((tasks) => {
+				const activeIndex = tasks.findIndex((t) => t.id === activeId);
+
+				tasks[activeIndex].columnId = overId;
+				console.log("DROPPING TASK OVER COLUMN", { activeIndex });
+				return arrayMove(tasks, activeIndex, activeIndex);
+			});
+		}
+	}
+}
+
+function generateId() {
+	/* Generate a random number between 0 and 10000 */
+	return Math.floor(Math.random() * 10001);
+}
+
+export default KanbanBoard;
