67a5d1f3e3fd0edca791325c09c211198a497cd9
omegamaster86 <masya868686@gmail.com>
2024-05-11 23:31:57 +0900

    アポイント獲得企業を表示

--- front/pages/apoint-list.tsx
+++ front/pages/apoint-list.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import KanbanBoard from "../components/ApointList/KanbanBoard"
+import { ApoList } from "../components/ApointList/ApointList"
+import { useCompanyAndKeyPersonsData } from "../components/CompanyList/useSWRCompanyList";
 
 
 const ApointList = () => {
   return (
     <div className='bg-cyan-400 min-h-screen'>
+      <ApoList/>
       <KanbanBoard/>
     </div>
   )
