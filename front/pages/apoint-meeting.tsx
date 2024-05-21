5e1e18b3679ff62c3fb1c3cc602749883de3b701
omegamaster86 <masya868686@gmail.com>
2024-05-21 23:32:24 +0900

    商談の記録ページを作成

--- front/pages/apoint-meeting.tsx
+++ front/pages/apoint-meeting.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { ApointMeetingRecord } from "../components/ApointList/ApointMeeting/ApointMeetingRecord"
+import { Chat } from "../components/ApointList/ApointMeeting/Chat"
 
 export default function ApointMeeting () {
   return (
-    <div>ApointMeeting</div>
+    <div className='m-10'>
+      <ApointMeetingRecord/>
+      <Chat />
+    </div>
   )
 }
